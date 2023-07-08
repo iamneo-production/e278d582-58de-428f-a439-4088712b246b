1c2629175c32b1a8e4e5594a432f24142478f3ca
Sonypriyasonu <19pa1a05e9@vishnu.edu.in>
2023-07-08 16:52:04 +0000

    query1

--- Airline_SonyPriya.sql
+++ Airline_SonyPriya.sql
@@ -1,4 +1,4 @@
-select * from AIRLINE;
 
-SELECT COUNT(ID) FROM AIRLINE WHERE GENDER='Female';
+/*Write a SQL query to find the number of female passengers who have made bookings for air tickets.*/
+SELECT COUNT(ID) as Female_Passenger_count FROM AIRLINE WHERE GENDER='Female';
 
