e42df9dde011ff1ee3987e968a3b09db4cf48fb4
Sonypriyasonu <61133478+Sonypriyasonu@users.noreply.github.com>
2023-07-09 00:17:05 +0530

    Update Airline_SonyPriya.sql

--- Airline_SonyPriya.sql
+++ Airline_SonyPriya.sql
@@ -6,10 +6,10 @@ SELECT COUNT(ID) as Female_Passenger_count FROM AIRLINE WHERE GENDER='Female';
 SELECT COUNT(ID) AS Passenger_count FROM AIRLINE WHERE CLASS='Business';
 
 --3. Write a sql query to find the number of passengers who have booked tickets in 'echo' class. 
-SELECT COUNT(ID) AS Echo_Passenger FROM AIRLINE WHERE CLASS='Echo';
+SELECT COUNT(ID) AS Echo_Passenger FROM AIRLINE WHERE CLASS='Eco';
 
 --4. Write a sql query to find the number of childrens who are all book the tickets childresn are below age 13.
 SELECT COUNT(ID) AS Kid_count FROM AIRLINE WHERE AGE<13;
 
 --5. Write a sql query to count of passengers who are above the age 16 and have booked tickets in business class.
-SELECT COUNT(ID) AS Adult_count FROM AIRLINE WHERE AGE<13 AND CLASS='Business';
+SELECT COUNT(ID) AS Adult_count FROM AIRLINE WHERE AGE<16 AND CLASS='Business';
