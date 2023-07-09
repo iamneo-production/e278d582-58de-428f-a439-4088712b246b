c645e7c956652588c1a1cb22f7c62a811e2f1be4
Sonypriyasonu <19pa1a05e9@vishnu.edu.in>
2023-07-09 11:15:59 +0000

    QUERY 5

--- Airline_SonyPriya.sql
+++ Airline_SonyPriya.sql
@@ -5,16 +5,12 @@ SELECT COUNT(ID) as Female_Passenger_count FROM AIRLINE WHERE GENDER='Female';
 --2. Write a sql query to find the total number of passengers who have booked tickets in the business class. 
 SELECT COUNT(ID) AS Passenger_count FROM AIRLINE WHERE CLASS='Business';
 
---3. Write a sql query to find the number of passengers who have booked tickets in 'echo' class. 
-SELECT COUNT(ID) AS Echo_Passenger FROM AIRLINE WHERE CLASS='Eco';
+--3. Write a sql query to find the number of passengers who have booked tickets in 'eco' class. 
+SELECT COUNT(ID) AS Eco_Passenger FROM AIRLINE WHERE CLASS='Eco';
 
 --4. Write a sql query to find the number of childrens who are all book the tickets childresn are below age 13.
 SELECT COUNT(ID) AS Kid_count FROM AIRLINE WHERE AGE<13;
 
 --5. Write a sql query to count of passengers who are above the age 16 and have booked tickets in business class.
-<<<<<<< HEAD
-SELECT COUNT(ID) AS Adult_count FROM AIRLINE WHERE AGE>16 AND CLASS='Business';
-=======
 SELECT COUNT(ID) AS Adult_count FROM AIRLINE WHERE AGE>16 AND CLASS='Business';
 
->>>>>>> 88b42d5a243c101d24e0050374a5438cd5c0c353
