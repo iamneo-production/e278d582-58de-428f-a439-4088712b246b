fcabb1b7785a95fce5ec0a3cd23fe849d10083a1
Pemmadi03 <mahalakshmi59505@gmail.com>
2023-07-09 14:58:11 +0000

    projectcode pemmadi

--- query-1.sql
+++ query-1.sql
@@ -7,4 +7,4 @@ select count(*) as No_of_Ecoclass_Passengers from airline where class='Eco';
 --query4
 select count(*) as No_of_ChildPassengers from airline where age<13;
 --query5
-select count(*) as No_of_Passengers_above16 from airline where age>16 and class='Business';
+select count(*) as No_of_Passengers_aboveage16 from airline where age>16 and class='Business';
