1d3b0917fcb0bd9e72c117d03020ee3f4b36f9ee
Pemmadi03 <mahalakshmi59505@gmail.com>
2023-07-10 16:41:02 +0000

    Maha changed query

--- query-1.sql
+++ query-1.sql
@@ -1,10 +1,10 @@
 --query1
-select count(*) as No_of_Female_Passengers from AIRLINE where GENDER='Female';
+select count(GENDER) as No_of_Female_Passengers from AIRLINE where GENDER='Female';
 --query2
-select count(*) as No_of_Businessclass_Passengers from airline where class ='Business';
+select count(CLASS) as No_of_Businessclass_Passengers from AIRLINE where CLASS ='Business';
 --query3
-select count(*) as No_of_Ecoclass_Passengers from airline where class='Eco';
+select count(CLASS) as No_of_Ecoclass_Passengers from AIRLINE where CLASS='Eco';
 --query4
-select count(*) as No_of_ChildPassengers from airline where age<13;
+select count(AGE) as No_of_ChildPassengers from AIRLINE where AGE<13;
 --query5
-select count(*) as No_of_Passengers_aboveage16 from airline where age>16 and class='Business';
+select count(AGE) as No_of_Passengers_aboveage16 from AIRLINE where AGE>16 and CLASS='Business';
