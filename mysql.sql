ddce3d027e2e2d3ef3bd1b700b13a09b0966ee54
pranaykasibhatta <pranaykasibhatla@gmail.com>
2023-07-10 11:54:53 +0000

    query 5

--- mysql.sql
+++ mysql.sql
@@ -1,5 +1,6 @@
 
---query to find no. of children who booked air tickets (age<13)
-select count(id) as Children_Count from airline 
-where Age<13;
 
+--query to find no. of passengers who are above 16 years and
+--booked business class
+select count(id) as Age_Passenger_Greater_Than_16 from airline
+where age>16 and class='Business';
