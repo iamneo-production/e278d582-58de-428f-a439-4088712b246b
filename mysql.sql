2435505c7265221f1a554a86b6bc09a93bb97e12
pranaykasibhatta <pranaykasibhatla@gmail.com>
2023-07-10 11:53:58 +0000

    query 3

--- mysql.sql
+++ mysql.sql
@@ -1,5 +1,3 @@
-
-
---query to find no. of passengers who booked air tickets in business class
-select count(id) as Business_class from airline where class='Business';
+- query to find no. of passengers who booked air tickets in eco class
+select count(id) as Eco_class from airline where class='Eco';
 
