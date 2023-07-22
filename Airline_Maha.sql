7564db7c493ea62879f58ab664a1fd576c8e381b
Pemmadi03 <132586226+Pemmadi03@users.noreply.github.com>
2023-07-22 19:15:41 +0530

    Update Airline_Maha.sql

--- Airline_Maha.sql
+++ Airline_Maha.sql
@@ -1,10 +1,10 @@
 --query1
-select count(*) as No_of_Female_Passengers from AIRLINE where GENDER='Female';
+select count(gender) as No_of_Female_Passengers from airline where gender='Female';
 --query2
-select count(*) as No_of_Businessclass_Passengers from airline where class ='Business';
+select count(class) as No_of_Businessclass_Passengers from airline where class ='Business';
 --query3
-select count(*) as No_of_Ecoclass_Passengers from airline where class='Eco';
+select count(class) as No_of_Ecoclass_Passengers from airline where class='Eco';
 --query4
-select count(*) as No_of_ChildPassengers from airline where age<13;
+select count(age) as No_of_ChildPassengers from airline where age<13;
 --query5
-select count(*) as No_of_Passengers_aboveage16 from airline where age>16 and class='Business';
+select count(class) as No_of_Passengers_aboveage16 from airline where age>16 and class='Business';
